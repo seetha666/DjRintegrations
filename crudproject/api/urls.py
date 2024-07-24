7ca24b70bc256485c79a3934bb1259ebbefa7270
seetha666 <seetha9456@gmail.com>
2024-07-24 11:55:41 +0530

    swagger facility added this project

--- crudproject/api/urls.py
+++ crudproject/api/urls.py
@@ -1,8 +1,27 @@
-from django.urls import path,include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path,include,re_path
 from api import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="This is Employee Api View",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@yourapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 from rest_framework import routers
 Router=routers.DefaultRouter()
 Router.register('employees',views.EmployeeViewL)
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('',include(Router.urls)),
 ]
--- crudproject/api/views.py
+++ crudproject/api/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from api.models import Employee
 from api.serializers import EmployeeSerializer
 from rest_framework import viewsets
+from rest_framework import views
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
+@swagger_auto_schema(
+        operation_description="Retrieve an example item",
+        responses={200: openapi.Response('Description', EmployeeSerializer)}
+    )
 class EmployeeViewL(viewsets.ModelViewSet):
     queryset=Employee.objects.all()
     serializer_class=EmployeeSerializer
